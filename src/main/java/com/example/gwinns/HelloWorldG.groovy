413bf4018c2c0f0c0e6922e2e0fe31299848522b
gregoryinns <gwinns@gmail.com>
2016-10-03 16:12:19 -0400

    Added two source files: one java, and one groovy.  just because.

---
+++ src/main/java/com/example/gwinns/HelloWorldG.groovy
@@ -0,0 +1,16 @@
+package com.example.gwinns
+
+/**
+ * Created by gwinns on 10/3/16.
+ */
+class HelloWorldG {
+
+    public static void main(String[] args) {
+
+        // notice: no System.out. in front of the println, and
+        // no semi-colon after the line;
+
+        println("Hello Greg, From Groovy")
+
+    }
+}
---
+++ src/main/java/com/example/gwinns/HelloWorldJ.java
@@ -0,0 +1,14 @@
+package com.example.gwinns;
+
+/**
+ * Created by gwinns on 10/3/16.
+ */
+public class HelloWorldJ {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Greg!  From pure Java.");
+
+    }
+
+}
