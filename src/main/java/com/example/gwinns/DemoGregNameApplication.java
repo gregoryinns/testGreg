c4542605d1efb8786047228755d2be4ce05a38bc
Gregory W. Inns <gwinns@gwinns-MacBookPro.local>
2016-10-03 16:01:57 -0400

    Initial commit

---
+++ src/main/java/com/example/gwinns/DemoGregNameApplication.java
@@ -0,0 +1,12 @@
+package com.example.gwinns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGregNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGregNameApplication.class, args);
+	}
+}
